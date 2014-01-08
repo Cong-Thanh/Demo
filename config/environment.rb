9c27003a88491e6c2fe58cc72cd62d067075b408
Thanh Cong Nguyen <ncthanh.engineer@gmail.com>
2014-01-08 15:19:27 -0600

    Switch to Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Demo::Application.initialize!
 
-Demo::Application.configure do 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { 
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com", 
-    authentication: "plain",
-    user_name: "thanh.nguyen.10008", 
-    password: "nguyencongthanh", 
-    enable_starttls_auto: true
-  }
-end
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
