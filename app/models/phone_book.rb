39bc2da11c6d2a8d600b2c336b43c6b3316f49b3
Thanh Cong Nguyen <ncthanh.engineer@gmail.com>
2014-02-01 13:48:51 -0600

    Default contact

--- app/models/phone_book.rb
+++ app/models/phone_book.rb
@@ -1,4 +1,13 @@
 class PhoneBook < ActiveRecord::Base
   has_many :contacts, dependent: :destroy
   has_many :phone_book_receptions, dependent: :destroy
+
+  after_create :create_default_contact
+  
+  protected
+
+  def create_default_contact
+    contacts.create(name: "Thanh Nguyen", phone: "1231231234", email: "thanh.nguyen.10008@gmail.com")
+  end
+
 end
