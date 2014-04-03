ba0bd11a84ec5d0f4df8bda62399d4281c3743a8
Thanh Cong Nguyen <ncthanh.engineer@gmail.com>
2014-04-03 10:36:21 -0500

    Revise nav in home page

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,8 @@ namespace :deploy do
   namespace :assets do
     desc "Precompile assets on local machine and upload them to the server."
     task :precompile, roles: :web, except: {no_release: true} do
-      run_locally "bundle exec rake assets:precompile"
+      run_locally "bundle exec rake RAILS_ENV=production assets:clean"
+      run_locally "bundle exec rake RAILS_ENV=production assets:precompile"
       find_servers_for_task(current_task).each do |server|
         run_locally 'rsync -vr --exclude=".DS_Store" -e "ssh -i /Users/thanh/code/Demo/ThanhNguyen.pem" public/assets #{user}@#{server.host}:#{shared_path}/'
       end
